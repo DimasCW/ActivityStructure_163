8248c1d4f100d46710307050da85caae9071d7cc
DimasCW <cdimas292@gmail.com>
2023-04-03 22:46:09 +0700

    menambahkan int main

--- Struct_1.cpp
+++ Struct_1.cpp
@@ -8,3 +8,14 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140193";
+	mhs1.nama = "Shenn";
+	mhs1.alamat = "Cirebon";
+	mhs1.umur = 20;
+
+
