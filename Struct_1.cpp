60d2b627cb36721222cbec17a305ec7270bbb29d
DimasCW <cdimas292@gmail.com>
2023-04-03 22:48:29 +0700

    menambahkan isi dari int main

--- Struct_1.cpp
+++ Struct_1.cpp
@@ -18,4 +18,12 @@ int main()
 	mhs1.alamat = "Cirebon";
 	mhs1.umur = 20;
 
+	cout << "Masukkan nim : ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur : ";
+	cin >> mhs2.umur;
 
